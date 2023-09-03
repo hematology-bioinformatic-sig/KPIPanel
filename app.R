15f3e209d3f04b0af5d3cb80274698175f6ba5ba
Yanhua Zheng <36413792+rainoffallingstar@users.noreply.github.com>
2023-09-03 10:52:50 +0800

    update plots

--- app.R
+++ app.R
@@ -14,6 +14,10 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 library(tidyr)
+library(showtext)
+library(DT)
+library(forcats)
+showtext_auto(enable = TRUE)
 #i=1
 #vector_list <- list(agust$mon,agust$tue,agust$wed,agust$thur,agust$fri,agust$sat,agust$sun)
 #students <- c()
@@ -25,7 +29,7 @@ library(tidyr)
 #}
 # 管红林
 
-# Jan；Feb；Mar；Apr； May； Jun；Jul； Aug；Sept；Oct；Nov；Dec
+month_list <- c('jan','feb','mar','apr','may','jun','jul','aug','sept','oct','nov','dec')
 normal_students <- c( "王子怡","管红琳","王刘宇","薛雨杭","张萌","葛睿",
                       "周雪榕","郭益欣","付晓研","刘彤","焦宇","王爽","杜宇昂",
                       "刘文晗" ,"丁心","张露丹","孟祥月","郑焱华")
@@ -110,11 +114,13 @@ week_summary <- function(batch_table,name_id,mod,month_data_list,types){
     
     weekdaysscore[[months_item]] <- data.frame(types = types,
                            temp = temp) %>% 
-      rename(months_item = temp)
+      rename(months_item = temp) 
   }
   weekdaysscore <- do.call("cbind",weekdaysscore) %>% 
     select(paste0(month_data_list[1],".types"),ends_with("months_item"))
   names(weekdaysscore) <- c("types",month_data_list)
+  weekdaysscore <- weekdaysscore %>% 
+    mutate(id =name_id)
   return(weekdaysscore)
 }
 
@@ -129,12 +135,30 @@ ui <- fluidPage(
         sidebarPanel(
             textInput("name_id",
                         "Whoes KPI you want to see:",
-                        "王子怡")
+                        "王子怡"),
+            selectInput("month_id",
+                      "Which month you want to know:",
+                      month_list,
+                      "aug")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          tabsetPanel(
+            tabPanel(title = "个人面板",
+                     plotOutput("distPlot"),
+                     tableOutput("selftable")
+            ),
+            tabPanel(title = "月排行榜",
+                     plotOutput("rankplot")
+            ),
+            tabPanel(title = "具体数据", 
+                     DTOutput("tables")
+            )
+          )
+          #plotOutput("distPlot"),
+           #plotOutput("rankplot"),
+           #tableOutput("tables")
         )
     )
 )
@@ -150,36 +174,69 @@ server <- function(input, output) {
     list.files("data/") %>% str_remove(".xlsx")
   })
   
-  weekday_summary <- reactive({
-    
-    weekday_summary <- week_summary(batch_table(),name_id = input$name_id,mod ="weekday" ,
-                 month_data_list = month_data_list() ,types = weekday_type_commm)
-    
+  rank_all <- reactive({
+    rank_df <- list()
+    for (name_id in normal_students) {
+      weekday_summary <- week_summary(batch_table(),name_id = name_id,mod ="weekday" ,
+                                      month_data_list = month_data_list() ,types = weekday_type_commm)
+      weekend_summary <- week_summary(batch_table(),name_id = name_id,mod ="weekend" ,
+                                      month_data_list = month_data_list() ,types = weekend_type_commm)
+      weeks_universe <- week_summary(batch_table(),name_id = name_id,mod ="universe" ,
+                                     month_data_list = month_data_list() ,types = universe_type)
+      df_templist <- list(weeks_universe,
+                          weekend_summary,
+                          weekday_summary)
+      rank_df[[name_id]] <-  data.table::rbindlist(df_templist,fill = TRUE) %>% 
+        gather(key = "month", value = "value", -types,-id)
+    }
+    rank_df <- data.table::rbindlist(rank_df,fill = TRUE)
   })
   
-  weekend_summary <- reactive({
-    
-    weekend_summary <- week_summary(batch_table(),name_id = input$name_id,mod ="weekend" ,
-                 month_data_list = month_data_list() ,types = weekend_type_commm)
-  })
-  
-  weeks_universe <- reactive({
-    
-    weeks_universe <- week_summary(batch_table(),name_id = input$name_id,mod ="universe" ,
-                 month_data_list = month_data_list() ,types = universe_type)
+  personal_table <- reactive({
+    df <- as.data.frame(rank_all())
+    df <- df %>% 
+      filter(id == input$name_id) %>% 
+      filter(month == input$month_id)
   })
 
     output$distPlot <- renderPlot({
-        df_templist <- list(weeks_universe(),
-                            weekend_summary(),
-                            weekday_summary())
-        df <- data.table::rbindlist(df_templist,fill = TRUE) %>% 
-          gather(key = "month", value = "value", -types)
+        df <- personal_table() %>% 
+          filter(value != 0)
         
         ggplot(df, aes(x = month, y = value, fill = types)) +
-          geom_bar(stat = "identity") +
-          labs(x = "month", y = "Values", title = "Monthly Data by Type") +
-          theme_minimal()
+          geom_bar(stat = "identity", position = "dodge") +
+          geom_text(aes(label = ifelse(value == 0, "", value)), 
+                    position = position_dodge(width = 0.9),vjust = -0.5) +
+                      labs(x = "Month", y = "Value", fill = "Types") +
+                      theme_bw()
+    })
+    output$selftable <- renderTable({
+      df <- personal_table() %>% 
+        select(-id) %>% 
+        arrange(-value)
+    })
+    
+    output$rankplot <- renderPlot({
+      df <- rank_all()
+      data_summary <- df %>%
+        group_by(id, month) %>%
+        summarize(total_value = sum(value)) %>%
+        filter(total_value != 0) %>% 
+        arrange(desc(total_value), desc(month))
+      
+      # 绘制条形图
+      ggplot(data_summary, aes(x = total_value, y = month, fill = id)) +
+        geom_bar(stat = "identity", position = "dodge") +
+        geom_text(aes(label = ifelse(total_value == 0, "", id)), position = position_dodge(width = 0.9), 
+                  vjust = -0.5) +
+        labs(x = "Value", y = "Month", fill = "ID") +
+        theme_bw()
+      
+      
+    })
+    
+    output$tables <-renderDT({
+      df <- rank_all()
     })
 }
 
